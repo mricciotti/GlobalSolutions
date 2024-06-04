16d2a8dc2a26e8b1d76950908f192a0d33d0f998
Matheus Ricciotti <matheus.ricciotti@gmail.com>
2024-06-04 18:01:09 -0300

    retirado a rolagem

--- javascritp/script.js
+++ javascritp/script.js
@@ -1,13 +1,4 @@
-const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      } else {
-        entry.target.classList.remove("show");
-      }
-    });
-  });
-  
+
   const elements = document.querySelectorAll(".rolagem");
   
   elements.forEach((element) => myObserver.observe(element));
